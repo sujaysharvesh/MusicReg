d3078a7a5a4641ae8a10153e7c25817bcf51d222
sujaySharvesh <sujaysharvesh098@gmail.com>
2025-04-26 19:19:51 +0530

    first commit

---
+++ src/test/java/com/example/server_Registry/ServerRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.server_Registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
